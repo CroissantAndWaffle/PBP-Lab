446cee448ce34ff41b28afad9a835c7a06af1de7
CroissantAndWaffle <elrafamaritza@gmail.com>
2022-10-06 16:33:56 +0700

    Lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -7,6 +7,8 @@ from wishlist.views import show_json_by_id
 from wishlist.views import register
 from wishlist.views import login_user
 from wishlist.views import logout_user
+from wishlist.views import wishlist_ajax
+
 
 app_name = 'wishlist'
 
@@ -19,4 +21,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('ajax/', wishlist_ajax, name='wishlist_ajax'),
+    # path('ajax/submit', ),
 ]
